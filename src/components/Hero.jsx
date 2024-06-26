1cfdf40e065f37a1907070c03efba9993da4e0e4
Hunde-D <hunddh2025@gmail.com>
2024-06-26 16:19:51 +0300

    active link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 import { useContext } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../context";
 const Hero = () => {
   const { searchValue, setSearchValue, handleSubmit } =
     useContext(GlobalContext);
-  const { pathname } = useLocation();
   return (
     <div className="title-bg relative mx-auto flex flex-col items-center justify-center p-2 text-center max-sm:w-5/6 lg:w-4/6">
       <h1 className="mt-32 text-5xl">Our Collection</h1>
@@ -32,13 +31,21 @@ const Hero = () => {
       <div className="space-x-6">
         <NavLink
           to="/"
-          className={`${pathname === "/" ? "bg-[#6F757C] font-semibold" : "border-current"} rounded-lg border px-2 py-1 text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in`}
+          className={({ isActive }) => {
+            return isActive
+              ? "rounded-lg border bg-[#6F757C] px-2 py-1 font-semibold text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in"
+              : "rounded-lg border border-current px-2 py-1 text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in";
+          }}
         >
           Home
         </NavLink>
         <NavLink
           to="favorites"
-          className={`${pathname === "/favorites" ? "bg-[#6F757C] font-semibold" : "border-current"} rounded-lg border px-2 py-1 text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in`}
+          className={({ isActive }) => {
+            return isActive
+              ? "rounded-lg border bg-[#6F757C] px-2 py-1 font-semibold text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in"
+              : "rounded-lg border border-current px-2 py-1 text-[#FEF7EE] transition-colors duration-[0.25sec] ease-in";
+          }}
         >
           Favorites
         </NavLink>
